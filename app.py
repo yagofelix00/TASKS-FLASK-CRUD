e5d5e077eb794d1d9885b9b3240aa17a208f64b3
Yago Felix <yagofelix007@gmail.com>
2025-10-16 11:25:48 -0300

    Implement POST /tasks route to create new tasks with auto-increment ID

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models import Task
 
 app = Flask(__name__)
@@ -8,12 +8,16 @@ app = Flask(__name__)
 # Tabela: Tarefa
 
 tasks = []
+task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Test'
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description',''))
+    task_id_control += 1
+    tasks.append(new_task)
+    return jsonify({'message': 'Nova tarefa criada com sucesso'})
 
-if __name__ == "__main)__":
+if __name__ == "__main__":
     app.run(debug=True)
